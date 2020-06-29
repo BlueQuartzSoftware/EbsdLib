da5a653b1c3b3f8a5575c1a2ff22d99990499751
Michael Jackson <mike.jackson@bluequartz.net>
2020-06-29 13:12:57 -0400

    Only use GHCFileSystem if GCC < 9 or MacOS
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/EbsdLib/SourceList.cmake
+++ Source/EbsdLib/SourceList.cmake
@@ -187,7 +187,7 @@ endif()
 # We are going to use an independent implementation of std::filesystem
 # that is used instead.
 # --------------------------------------------------------------------
-if(APPLE)
+if(NOT APPLE)
   target_link_libraries(${PROJECT_NAME} ghcFilesystem::ghc_filesystem)
 endif()
 
@@ -195,8 +195,9 @@ endif()
 # GCC Versions Less Than 9 need to link against stdc++fs is using std::filesystem
 # --------------------------------------------------------------------
 if(CMAKE_COMPILER_IS_GNUCC AND "${CMAKE_SYSTEM_NAME}" STREQUAL "Linux" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
-  message(STATUS "GCC Version < 9: Linking to stdc++fs")
-  target_link_libraries(${PROJECT_NAME} stdc++fs)
+  # message(STATUS "GCC Version < 9: Linking to stdc++fs")
+  # target_link_libraries(${PROJECT_NAME} stdc++fs)
+  target_link_libraries(${PROJECT_NAME} ghcFilesystem::ghc_filesystem)
 endif()
 
 # --------------------------------------------------------------------
