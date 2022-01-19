0ce17d8b3e52731e363cd98b8df30fba1da91cae
Jared Duffey <jared.duffey@bluequartz.net>
2022-01-19 17:22:16 -0500

    Added dependencies to cmake config file
    * Removed old Qt dependency from cmake config file
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Source/EbsdLib/cmake/EbsdLibConfig.cmake
+++ Source/EbsdLib/cmake/EbsdLibConfig.cmake
@@ -1,27 +1,14 @@
-#-------------------------------------------------------------------------------
-# Find the Qt5 Library as we need that.
-#-------------------------------------------------------------------------------
-set(Qt5_COMPONENTS "Core")
-# On Linux we need the DBus library
-if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-	set(Qt5_COMPONENTS ${Qt5_COMPONENTS} DBus)
+include(CMakeFindDependencyMacro)
+
+find_dependency(Eigen3)
+
+if(@EbsdLib_ENABLE_HDF5@)
+  find_dependency(H5Support)
 endif()
 
-find_package(Qt5 COMPONENTS ${Qt5_COMPONENTS})
-if(NOT Qt5_FOUND)
-	message(FATAL_ERROR "Qt5 is Required for EbsdLib to build. Please install it or set the Qt5_DIR cmake variable")
+if(@EbsdLib_USE_PARALLEL_ALGORITHMS@)
+  find_dependency(TBB)
 endif()
-
-#-------------------------------------------------------------------------------
-# Find the Eigen Library as we need that.
-#-------------------------------------------------------------------------------
-Find_Package(Eigen3 REQUIRED)
-if(NOT EIGEN3_FOUND)
-  message(FATAL_ERROR "Eigen 3 is Required for EbsdLib to build. Please install it or set the Eigen3_DIR variable")
-endif()
-
-message(STATUS "* Qt5 (${Qt5_VERSION}) ${Qt5_DIR}")
-message(STATUS "* Eigen (${EIGEN3_VERSION_STRING}) ${Eigen3_DIR}")
 
 include("${CMAKE_CURRENT_LIST_DIR}/EbsdLibTargets.cmake")
 
