a78f44f95474ad29ccfe205f64a213d1fb8b2f3b
Jared Duffey <jared.duffey@bluequartz.net>
2022-01-19 22:51:16 -0500

    Added install interface include directories
    * Removed H5Support_INCLUDE_DIRS since the include directories
    should come from the target
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Source/EbsdLib/SourceList.cmake
+++ Source/EbsdLib/SourceList.cmake
@@ -147,7 +147,7 @@ target_include_directories(${PROJECT_NAME}
   PUBLIC
     $<BUILD_INTERFACE:${EbsdLibProj_SOURCE_DIR}/Source>
     $<BUILD_INTERFACE:${EbsdLibProj_BINARY_DIR}>
-    ${H5Support_INCLUDE_DIRS}
+    $<INSTALL_INTERFACE:include>
 )
 target_link_libraries(${PROJECT_NAME}
   PUBLIC
