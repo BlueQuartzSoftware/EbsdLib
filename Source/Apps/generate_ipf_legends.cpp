a0484d07aaceb55e1581bf808644f7f1d2e3b6f3
Michael Jackson <mike.jackson@bluequartz.net>
2024-09-18 18:06:41 -0400

    Clang format
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/Apps/generate_ipf_legends.cpp
+++ Source/Apps/generate_ipf_legends.cpp
@@ -34,7 +34,7 @@
 using namespace EbsdLib;
 
 const std::string k_Output_Dir(UnitTest::DataDir + "IPF_Legend/");
-//const std::string k_Output_Dir(UnitTest::TestTempDir + "IPF_Legend/");
+// const std::string k_Output_Dir(UnitTest::TestTempDir + "IPF_Legend/");
 
 using EbsdDoubleArrayType = EbsdDataArray<float>;
 using EbsdDoubleArrayPointerType = EbsdDoubleArrayType::Pointer;
