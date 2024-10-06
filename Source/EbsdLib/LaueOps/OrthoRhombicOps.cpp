ff10e81c28dfd69a6158945ca8df89a1bd880b39
Michael Jackson <mike.jackson@bluequartz.net>
2024-10-06 12:32:38 -0400

    COMP: Add missing #include directive for tbb task_group
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/EbsdLib/LaueOps/OrthoRhombicOps.cpp
+++ Source/EbsdLib/LaueOps/OrthoRhombicOps.cpp
@@ -49,6 +49,7 @@
 #ifdef EbsdLib_USE_PARALLEL_ALGORITHMS
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
+#include <tbb/task_group.h>
 #endif
 
 #define EBSD_LIB_GENERATE_ENTIRE_CIRCLE
