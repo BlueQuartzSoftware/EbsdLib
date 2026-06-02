2edbdb25621763889b5488f7d37057d63cbade7d
Michael Jackson <mike.jackson@bluequartz.net>
2026-06-02 11:05:25 -0400

    CMAKE: Add all EbsdLib executables to their own group.
    This helps IDEs better organize their project structures

--- Source/Apps/SourceList.cmake
+++ Source/Apps/SourceList.cmake
@@ -4,23 +4,29 @@ configure_file(${EbsdLibProj_SOURCE_DIR}/Source/Test/TestFileLocations.h.in
 add_executable(rotconvert ${EbsdLibProj_SOURCE_DIR}/Source/Apps/rotconvert.cpp)
 target_link_libraries(rotconvert PUBLIC EbsdLib)
 target_include_directories(rotconvert PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(rotconvert PROPERTIES FOLDER EbsdLib_Applications)
+
 
 add_executable(make_ipf ${EbsdLibProj_SOURCE_DIR}/Source/Apps/make_ipf.cpp)
 target_link_libraries(make_ipf PUBLIC EbsdLib)
 target_include_directories(make_ipf PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(make_ipf PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(convert_orientations ${EbsdLibProj_SOURCE_DIR}/Source/Apps/ConvertOrientations.cpp)
 target_link_libraries(convert_orientations PUBLIC EbsdLib)
 target_include_directories(convert_orientations PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(convert_orientations PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(gen_sym_code ${EbsdLibProj_SOURCE_DIR}/Source/Apps/gen_sym_code.cpp)
 target_link_libraries(gen_sym_code PUBLIC EbsdLib)
 target_include_directories(gen_sym_code PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(gen_sym_code PROPERTIES FOLDER EbsdLib_Applications)
 
 
 add_executable(eq_orientations ${EbsdLibProj_SOURCE_DIR}/Source/Apps/eq_orientations.cpp)
 target_link_libraries(eq_orientations PUBLIC EbsdLib)
 target_include_directories(eq_orientations PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(eq_orientations PROPERTIES FOLDER EbsdLib_Applications)
 
 
 add_executable(generate_ipf_legends ${EbsdLibProj_SOURCE_DIR}/Source/Apps/generate_ipf_legends.cpp)
@@ -29,6 +35,7 @@ target_include_directories(generate_ipf_legends
     PUBLIC
         ${EbsdLibProj_SOURCE_DIR}/Source
         ${EbsdLibProj_BINARY_DIR})
+set_target_properties(generate_ipf_legends PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(generate_ipf_density ${EbsdLibProj_SOURCE_DIR}/Source/Apps/generate_ipf_density.cpp)
 target_link_libraries(generate_ipf_density PUBLIC EbsdLib)
@@ -36,18 +43,22 @@ target_include_directories(generate_ipf_density
     PUBLIC
         ${EbsdLibProj_SOURCE_DIR}/Source
         ${EbsdLibProj_BINARY_DIR})
+set_target_properties(generate_ipf_density PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(generate_ipf_from_file ${EbsdLibProj_SOURCE_DIR}/Source/Apps/generate_ipf_from_file.cpp)
 target_link_libraries(generate_ipf_from_file PUBLIC EbsdLib)
 target_include_directories(generate_ipf_from_file PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(generate_ipf_from_file PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(generate_pole_figure ${EbsdLibProj_SOURCE_DIR}/Source/Apps/generate_pole_figure.cpp)
 target_link_libraries(generate_pole_figure PUBLIC EbsdLib)
 target_include_directories(generate_pole_figure PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(generate_pole_figure PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(make_pole_figure ${EbsdLibProj_SOURCE_DIR}/Source/Apps/make_pole_figure.cpp)
 target_link_libraries(make_pole_figure PUBLIC EbsdLib)
 target_include_directories(make_pole_figure PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(make_pole_figure PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(render_ebsd
   ${EbsdLibProj_SOURCE_DIR}/Source/Apps/render_ebsd.cpp
@@ -55,10 +66,12 @@ add_executable(render_ebsd
   ${EbsdLibProj_SOURCE_DIR}/Source/Apps/render_ebsd_main.cpp)
 target_link_libraries(render_ebsd PUBLIC EbsdLib)
 target_include_directories(render_ebsd PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(render_ebsd PROPERTIES FOLDER EbsdLib_Applications)
 
 add_executable(ParseAztecProject ${EbsdLibProj_SOURCE_DIR}/Source/Apps/ParseAztecProject.cpp)
 target_link_libraries(ParseAztecProject PUBLIC EbsdLib)
 target_include_directories(ParseAztecProject PUBLIC ${EbsdLibProj_SOURCE_DIR}/Source)
+set_target_properties(ParseAztecProject PROPERTIES FOLDER EbsdLib_Applications)
 
 
 if(EbsdLib_INSTALL_FILES)
